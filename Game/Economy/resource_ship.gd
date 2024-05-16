b9eb6a87ca00ff783b7bc3197b1293da640ceeda
Yağız Boğa <boga.yagiz@gmail.com>
2024-05-16 22:38:07 +0300

    added function for path2d
    it hasn't got curve yet, it does not goes exactly to the specified coordinates

--- Game/Economy/resource_ship.gd
+++ Game/Economy/resource_ship.gd
@@ -1,8 +1,32 @@
 extends Node2D
 
-@onready var path_follow = $Path2D/PathFollow2D
+var new_path:Path2D
+var new_path_follow:PathFollow2D
+var resource_ship_sprite:Sprite2D
 
-
-
+func start_collecting(start_position:Vector2,destination_position:Vector2):
+	new_path = Path2D.new()
+	new_path_follow = PathFollow2D.new()
+	new_path.curve = Curve2D.new()
+	resource_ship_sprite = Sprite2D.new()
+	
+	add_child(new_path)
+	new_path.add_child(new_path_follow)
+	new_path_follow.add_child(resource_ship_sprite)
+	
+	resource_ship_sprite.texture = load("res://Sprites/uzaygemisi/resource ships/resource_ship.png")
+	resource_ship_sprite.scale *= 0.1
+	resource_ship_sprite.rotate(1.5)
+	
+	new_path.curve.add_point(start_position)
+	new_path.curve.add_point(destination_position)
+	new_path.curve.add_point(start_position)
+	
 func _process(delta):
-	path_follow.progress += 100 * delta
+	if new_path_follow:
+		new_path_follow.progress+=100 * delta
+
+
+	
+	
+	
--- Game/Stations/StationInteraction.gd
+++ Game/Stations/StationInteraction.gd
@@ -2,26 +2,23 @@ extends Node2D
 #Github
 @onready var body_entered : bool = false
 @onready var dialogue = $CanvasLayer
-@onready var path_follow = $"../resource_ship/resource_ship_path/PathFollow2D"
-@onready var resource_ship = $"../resource_ship/resource_ship_path/PathFollow2D/Sprite2D"
+@onready var resource_ship = $"../resource_ship"
+@onready var flag = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if body_entered == true:
-		path_follow.progress += 70 * delta
-		resource_ship.show()
-		
-
 
 
 func _on_area_2d_body_entered(body):
 	if(body.name == "Character"):
 		dialogue.show()
 		body_entered = true
-		#resource ship movement
+		if flag:
+			resource_ship.start_collecting(Vector2(570,122),Vector2(500,1000))
+			flag = false
+		
 		
 		
