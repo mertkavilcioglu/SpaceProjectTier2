08a6c4b68771db1225e3d5e0c3508e861112ba92
Yağız Boğa <boga.yagiz@gmail.com>
2024-06-10 14:48:06 +0300

    enemy types update

--- Game/Enemy/kamikaze.gd
+++ Game/Enemy/kamikaze.gd
@@ -3,13 +3,15 @@ extends Node2D
 @onready var character = $"../Character"
 var target_position
 var distance_to_character
+
 func _ready():
-	if distance_to_character <=500:
+	if distance_to_character == 300:
 		target_position = character.global_position
+
 func _process(delta):
 	distance_to_character = global_position.distance_to(character.global_position)
-	if distance_to_character > 500:
+	if distance_to_character > 300:
 		global_position = global_position.lerp(character.global_position,delta)
-	elif distance_to_character <= 500:
+	elif distance_to_character == 300:
 		global_position = global_position.lerp(target_position,delta)
 
