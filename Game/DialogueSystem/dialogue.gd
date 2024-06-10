2a5844ed3345af505627cd33b54f9adcdf616854
Yağız Boğa <boga.yagiz@gmail.com>
2024-06-10 21:42:41 +0300

    dialog box update
    requirements:
    game pause on dialogue start
    game start on dialogue finish

--- Game/DialogueSystem/dialogue.gd
+++ Game/DialogueSystem/dialogue.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 @export var card_image : Sprite2D
 @export var profile_image : Sprite2D
@@ -6,34 +6,32 @@ extends Node
 @export var left_text : RichTextLabel
 @export var dialogue_text : RichTextLabel
 
-
 var dialogues
 var current_dialogue
 
 func _ready():
-	dialogues = load_dialogue("res://Game/DialogueSystem/Dialogues.json")
-	current_dialogue =  dialogues["first_question"]
+	dialogues = load_dialogue("res://Game/DialogueSystem/mission_dialogues/mission1_part1.json")
+	current_dialogue =  dialogues["text1"]
 	update_card()
-
+	
 func load_dialogue(file_path:String):
 	var data_file = FileAccess.open(file_path , FileAccess.READ)
 	var parsed = JSON.parse_string(data_file.get_as_text())
 	return parsed
 	
-func select_left():
-	if "left_result" in current_dialogue:
-		current_dialogue = dialogues[current_dialogue["left_result"]]
-	update_card()
-func select_right():
-	if "right_result" in current_dialogue:
-		current_dialogue = dialogues[current_dialogue["right_result"]]
-	update_card()
-
 func update_card():
 	right_text.text = current_dialogue["right_answer"]
 	left_text.text = current_dialogue["left_answer"]
 	dialogue_text.text = current_dialogue["question"]
 	card_image.texture = load(current_dialogue["card_image"])
 	profile_image.texture = load(current_dialogue["profile_image"])
-	
 
+func select_left():
+	if "left_result" in current_dialogue:
+		current_dialogue = dialogues[current_dialogue["left_result"]]
+	update_card()
+	
+func select_right():
+	if "right_result" in current_dialogue:
+		current_dialogue = dialogues[current_dialogue["right_result"]]
+	update_card()
--- Game/Stations/StationInteraction.gd
+++ Game/Stations/StationInteraction.gd
@@ -2,8 +2,10 @@ extends Node2D
 #Github
 @onready var body_entered : bool = false
 @onready var dialogue = $CanvasLayer
+@onready var dialogue_screen = $CanvasLayer/Dialogue
 @onready var resource_ship = $"../resource_ship"
 @onready var flag = true
+@onready var game = $".."
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
