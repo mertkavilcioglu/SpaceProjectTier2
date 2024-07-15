6038739c97c296d1a876c35e3a6ff0c191c08414
Yağız Boğa <boga.yagiz@gmail.com>
2024-07-15 18:01:13 +0300

    game design,dialogues

--- Game/mission_vector.gd
+++ Game/mission_vector.gd
@@ -1,8 +1,10 @@
 extends Sprite2D
 
 @onready var station1 = $"../karagünes_atolyesi"
-@onready var station2 = $"../Station2"
-@onready var station3 = $"../Station3"
+@onready var station2 = $"../korsan_istasyonu"
+@onready var station3 = $"../themis"
+@onready var station4 = $"../iroh"
+@onready var station5 = $"../devrimci"
 @onready var character = $"../Character"
 var camera
 var screen_center
@@ -17,24 +19,27 @@ func _process(delta):
 	show()
 	if station1.body_entered == true:
 		hide()
-	#	if station1.dialogue.visible == true:
-		#	counter = 2
-		#if station2.dialogue.visible == true:
-		#	counter = 3
-		#elif station3.dialogue.visible == true:
-		#	counter = 4
+		if station1.dialogue.visible == true:
+			counter = 2
+		elif station2.dialogue.visible == true:
+			counter = 3
+		elif station3.dialogue.visible == true:
+			counter = 4
+		elif station4.dialogue.visible == true:
+			counter =5
 
 
 func station_changer():
 	if counter == 1:
 		station_position_path = station1.global_position
-	#elif counter == 2:
-		#station_position_path = station2.global_position
-	#elif counter == 3:
-		#station_position_path = station3.global_position
-	#elif counter == 4:
-	#	station_position_path = Vector2(0,0)
-		hide()
+	elif counter == 2:
+		station_position_path = station2.global_position
+	elif counter == 3:
+		station_position_path = station3.global_position
+	elif counter == 4:
+		station_position_path = station4.global_position
+	elif counter == 5:
+		station_position_path = station5.global_position
 	
 	
 func direct_to_mission(target_position):
